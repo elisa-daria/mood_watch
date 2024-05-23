998c31da65bd366dc50b985107119443247a70e7
elisa-daria <elisa.daria.tomasi@gmail.com>
2024-05-24 00:26:32 +0200

    adds-button-playground

--- src/components/queerdom/TagContainer.jsx
+++ src/components/queerdom/TagContainer.jsx
@@ -8,36 +8,44 @@ const TagContainer = () => {
     <Container fluid className="flex-grow-1">
       <Row>
         <Col xs={12}>
-          <section className="bg-bg-main">
-            <div className="position-relative bg-dark-grey">
+          <section className="bg-bg-main bg-dark-grey playground">
+            <div className="position-relative wrapper ">
               <div className="position-absolute top-0 start-0">
-                <Button variant="red" size="lg">
-                  Let's go
+                <Button variant="red" size="lg" className="playground-btn">
+                  no lesbian dies
                 </Button>
               </div>
               <div className="position-absolute top-0 end-0">
-                <Button variant="orange" size="lg">
-                  Let's go
+                <Button
+                  variant="orange"
+                  size="lg"
+                  className="playground-btn  d-none d-sm-block"
+                >
+                  Garbo classic
                 </Button>
               </div>
               <div className="position-absolute top-50 start-50">
-                <Button variant="yellow" size="lg">
-                  Let's go
+                <Button variant="yellow" size="lg" className="playground-btn">
+                  baby gays
                 </Button>
               </div>
               <div className="position-absolute bottom-50 end-50">
-                <Button variant="green" size="lg">
-                  Let's go
+                <Button variant="green" size="lg" className="playground-btn">
+                  should've been more gay
                 </Button>
               </div>
               <div className="position-absolute bottom-0 start-0">
-                <Button variant="blue" size="lg">
-                  Let's go
+                <Button
+                  variant="blue"
+                  size="lg"
+                  className="playground-btn d-none d-sm-block"
+                >
+                  fruity juice
                 </Button>
               </div>
               <div className="position-absolute bottom-0 end-0">
-                <Button variant="violet" size="lg">
-                  Let's go
+                <Button variant="violet" size="lg" className="playground-btn">
+                  hidden in the closet
                 </Button>
               </div>
             </div>
